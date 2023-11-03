a5592a29e4170ae20511cdc0857d6e46837125c4
chhamilton000 <106551242+chhamilton000@users.noreply.github.com>
2023-11-03 11:13:33 -0500

    Folder Structuring

---
+++ RoyalSocial/Protocols/AuthenticationFormProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  AuthenticationFormProtocol.swift
+//  RoyalSocial
+//
+//  Created by Caley Hamilton on 11/3/23.
+//
+
+import Foundation
+
+protocol AuthenticationFormProtocol {
+    var formIsValid: Bool { get }
+}
